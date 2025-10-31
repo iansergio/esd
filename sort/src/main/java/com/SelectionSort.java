c055ad3cc8446f808d189dc2d5b1d77a9c3f5eb2
Ian Sergio Helfenberger <iansergiohelf@gmail.com>
2025-10-31 13:31:47 -0300

    selection sort

--- sort/src/main/java/com/SelectionSort.java
+++ sort/src/main/java/com/SelectionSort.java
@@ -2,25 +2,26 @@ package com;
 
 public class SelectionSort {
     public static void main(String[] args) {
-
-        int[] list = { 25, 20, 22, 11, 8, 18 };
-        
-        int smallest = list[0];
-
-        for (int i = 1; i < list.length; i++) {
-            for (int j = 1; j < list.length; j++) {
-                if (list[j] < smallest) {
-                    smallest = list[j];
-
-                    
+       
+        int[] array = {25, 20, 22, 11, 8, 18};
+       
+        for (int i = 0; i < array.length; i++) {
+           
+            int indexAux = i;
+           
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[indexAux]) {
+                    indexAux = j;
                 }
             }
+           
+            int aux = array[i];
+            array[i] = array[indexAux];
+            array[indexAux] = aux;
         }
 
-        System.out.println(smallest);
-
-        for (int i = 0; i < list.length; i++) {
-            System.out.print(list[i] + " ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
     }
 }
